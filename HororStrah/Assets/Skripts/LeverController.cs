b6bece0af56c9b801ae85cfff0be72055a0b501b
Sega <vitaliy.tkatcsch@gmail.com>
2024-11-02 13:00:58 +0300

    Новые рычаги

--- HororStrah/Assets/Skripts/LeverController.cs
+++ HororStrah/Assets/Skripts/LeverController.cs
@@ -3,46 +3,38 @@ using UnityEngine;
 public class LeverController : MonoBehaviour
 {
     [SerializeField] private Animator leverAnimator;
+    [SerializeField] private float interactionDistance = 2f; // Расстояние взаимодействия
     private bool isActivated = false;
-    private bool playerInRange = false;
+    private Camera mainCamera;
 
     public bool IsActivated { get { return isActivated; } }
 
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !isActivated)
         {
-            Debug.Log($"Кнопка E нажата. PlayerInRange: {playerInRange}, IsActivated: {isActivated}");
-        }
+            // Создаем луч из центра камеры
+            Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+            RaycastHit hit;
 
-        if (playerInRange && Input.GetKeyDown(KeyCode.E) && !isActivated)
-        {
-            Debug.Log("Пытаемся активировать рычаг");
-            ActivateLever();
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log($"Что-то вошло в триггер: {other.gameObject.name} с тегом: {other.tag}");
-
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Игрок вошел в зону рычага");
-            playerInRange = true;
+            // Проверяем, попал ли луч в объект и находится ли он в пределах дистанции взаимодействия
+            if (Physics.Raycast(ray, out hit, interactionDistance))
+            {
+                // Проверяем, попали ли мы в этот рычаг
+                if (hit.collider.gameObject == gameObject)
+                {
+                    Debug.Log("Пытаемся активировать рычаг");
+                    ActivateLever();
+                }
+            }
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Игрок вышел из зоны рычага");
-            playerInRange = false;
-        }
-    }
-
-    // Только ОДИН метод ActivateLever должен быть в классе
     private void ActivateLever()
     {
         Debug.Log("Активация рычага");
